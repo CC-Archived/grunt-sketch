224bb3fe1f0abb8182fed225ad802262a3695f04
John Yanarella <jmy@codecatalyst.com>
2015-02-05 16:38:34 -0500

    Updated version metadata.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
- * [grunt-sketch](http://github.com/CodeCatalyst/grunt-sketch) v1.0.1
- * Copyright (c) 2014 [CodeCatalyst, LLC](http://www.codecatalyst.com/).
+ * [grunt-sketch](http://github.com/CodeCatalyst/grunt-sketch) v1.0.2
+ * Copyright (c) 2014-2015 [CodeCatalyst, LLC](http://www.codecatalyst.com/).
  * Open source under the [MIT License](http://en.wikipedia.org/wiki/MIT_License).
  */
 
